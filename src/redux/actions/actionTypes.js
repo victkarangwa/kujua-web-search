9807ce51b4fd820480c2e08bf60b6fee4b40ea86
Victor Karangwa <victorkarangwa4@gmail.com>
2019-12-18 11:17:45 +0200

    feat(results): add search results page
    Add picturebox for the results

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,3 @@
+export const SEARCH_KEY = 'SEARCH_KEY';
+export const QUERY_RESULTS = 'QUERY_RESULTS';
+export const QUERY_LINK = 'QUERY_LINK';
---
+++ src/redux/reducers/queries.js
@@ -0,0 +1,27 @@
+const initialState = {
+      query: 'Hello world'  
+    }
+const queries = (state = initialState, action) =>{
+    switch (action.type) {
+        case 'SEARCH_KEY':
+            return {
+                ...state,
+                query: action.query
+            }
+        case 'QUERY_RESULTS':
+            return {
+                ...state,
+                results: action.results
+            }
+        case 'QUERY_LINK':
+            return {
+                ...state,
+                clicked: action.clicked
+            }
+
+        default:
+            return state
+    }
+}
+
+export default queries; 
