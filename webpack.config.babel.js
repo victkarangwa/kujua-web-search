fca54bc472db5e628c2adf0c985cbdf650daff5d
Victor Karangwa <victorkarangwa4@gmail.com>
2020-03-02 12:55:22 +0200

    bg(spinner): fix spinner - Fix link shortner api [Finishes #171569293]

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,41 +5,45 @@ const path = require('path');
 
 dotenv.config();
 module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, 'src/index'),
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
-            }
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        include: path.resolve(
+          __dirname,
+          './node_modules/react-alice-carousel/lib/alice-carousel.css'
+        ),
+        test: /\.(s[ac]ss|css)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
         ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        
-        
-    ],
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public/'),
-        publicPath: "http://localhost:3000/dist",
-        historyApiFallback: true,
-        port: process.env.PORT || 7000,
-        hotOnly: true
-    },
-}
+      }
+    ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public/'),
+    publicPath: 'http://localhost:3000/dist',
+    historyApiFallback: true,
+    port: process.env.PORT || 7000,
+    hotOnly: true
+  }
+};
